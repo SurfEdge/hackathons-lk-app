400f31d030512efc17b011a6c2af346b6cc5986c
Thaanu Perera <thaanup@gmail.com>
2021-05-08 16:02:19 +0530

    Add all events card

--- lib/screens/events_screen.dart
+++ lib/screens/events_screen.dart
@@ -3,6 +3,63 @@ import 'package:hackathons_lk_app/sections/all_events_section.dart';
 import 'package:hackathons_lk_app/sections/ended_events_section.dart';
 import 'package:hackathons_lk_app/sections/upcoming_events_section.dart';
 import 'package:hackathons_lk_app/services/scroll_glow_disabler.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<List<Data>> fetchData() async {
+  final response = await http
+      .get(Uri.parse('https://hackathons.lk/wp-json/wp/v2/event?_embed'));
+  if (response.statusCode == 200) {
+    List jsonResponse = json.decode(response.body);
+    return jsonResponse.map((data) => new Data.fromJson(data)).toList();
+  } else {
+    throw Exception('Unexpected error occured!');
+  }
+}
+
+class Data {
+  final String name;
+  final String title;
+  final String eventLocation;
+  final String eventStartDate;
+  final String eventStartTime;
+  final String eventEndDate;
+  final String eventEndTime;
+  final String status;
+  final String website;
+  final String image;
+  final String views;
+
+  Data(
+      {this.name,
+      this.title,
+      this.eventLocation,
+      this.eventStartDate,
+      this.eventStartTime,
+      this.eventEndDate,
+      this.eventEndTime,
+      this.status,
+      this.website,
+      this.image,
+      this.views});
+
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return Data(
+      name: json['name'],
+      title: json['title']['rendered'].toString(),
+      eventLocation: json['event_location'],
+      eventStartDate: json['event_start_date'],
+      eventStartTime: json['event_start_time'],
+      eventEndDate: json['event_end_date'],
+      eventEndTime: json['event_end_time'],
+      status: (json['event_status'] == 'upcoming') ? 'Upcoming' : 'Ended',
+      website: json['event_website'],
+      image: json['_embedded']['wp:featuredmedia'][0]['media_details']['sizes']
+          ['medium_large']['source_url'],
+      views: json['post_views_count'],
+    );
+  }
+}
 
 class EventsScreen extends StatefulWidget {
   @override
@@ -10,20 +67,30 @@ class EventsScreen extends StatefulWidget {
 }
 
 class _EventsScreenState extends State<EventsScreen> {
+  Future<List<Data>> eventData;
   int _tabIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    eventData = fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.fromLTRB(20, 50, 20, 0),
+        padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             //* Hackathon.lk logo -------------------------------------------------------------------------------
-            Image.asset(
-              'lib/assets/Hackathons.lk-logo-black.png',
-              width: 220,
+            Container(
+              padding: EdgeInsets.only(left: 20),
+              child: Image.asset(
+                'lib/assets/Hackathons.lk-logo-black.png',
+                width: 220,
+              ),
             ),
             SizedBox(
               height: 12,
@@ -75,7 +142,9 @@ class _EventsScreenState extends State<EventsScreen> {
                           child: IndexedStack(
                             index: _tabIndex,
                             children: <Widget>[
-                              AllEventsSection(), //* All Events Section
+                              AllEventsSection(
+                                eventData: eventData,
+                              ), //* All Events Section
                               UpcomingEventsSection(), //* Upcoming Events Section
                               EndedEventsSection(), //* Ended Events Section
                             ],
--- lib/sections/all_events_section.dart
+++ lib/sections/all_events_section.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hackathons_lk_app/screens/events_screen.dart';
+import 'package:hackathons_lk_app/services/customicons_icons.dart';
+import 'package:intl/intl.dart';
 
 class AllEventsSection extends StatefulWidget {
+  final Future<List<Data>> eventData;
+  AllEventsSection({@required this.eventData});
+
   @override
   _AllEventsSectionState createState() => _AllEventsSectionState();
 }
@@ -8,6 +14,194 @@ class AllEventsSection extends StatefulWidget {
 class _AllEventsSectionState extends State<AllEventsSection> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: FutureBuilder(
+        future: widget.eventData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Data> data = snapshot.data;
+            print(data[0].image);
+            return ListView.builder(
+              padding: EdgeInsets.zero,
+              // shrinkWrap: true,
+              // physics: NeverScrollableScrollPhysics(),
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                  //* Main Card shadow
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0),
+                    // the box shawdow property allows for fine tuning as aposed to shadowColor
+                    boxShadow: [
+                      new BoxShadow(
+                        color: Colors.black54,
+                        // offset, the X,Y coordinates to offset the shadow
+                        offset: new Offset(5.0, 5.0),
+                        // blurRadius, the higher the number the more smeared look
+                        blurRadius: 36.0,
+                        spreadRadius: -23,
+                      )
+                    ],
+                  ),
+                  //* Event Card ----------------------------------------------------------------------
+                  child: Card(
+                    elevation: 0,
+                    margin: EdgeInsets.zero,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.only(bottom: 5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20)),
+                            child: Stack(
+                              alignment: Alignment.bottomLeft,
+                              children: [
+                                //* Image
+                                Image.network(
+                                  data[index].image,
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    //* Event Status
+                                    Container(
+                                      margin: EdgeInsets.only(bottom: 10),
+                                      padding: EdgeInsets.fromLTRB(6, 4, 6, 4),
+                                      color: Color(0xff1976D2),
+                                      child: Text(
+                                        data[index].status,
+                                        style: TextStyle(
+                                          fontFamily: 'poppins',
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.w400,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                    //* Event Views
+                                    Container(
+                                      margin: EdgeInsets.only(
+                                          bottom: 10, right: 10),
+                                      padding: EdgeInsets.fromLTRB(8, 4, 8, 4),
+                                      decoration: BoxDecoration(
+                                        color: Colors.black87,
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                      child: Row(
+                                        children: [
+                                          Icon(
+                                            Icons.remove_red_eye_rounded,
+                                            color: Colors.white,
+                                            size: 14,
+                                          ),
+                                          SizedBox(width: 5),
+                                          Text(
+                                            data[index].views,
+                                            style: TextStyle(
+                                              fontFamily: 'poppins',
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.w400,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(15, 5, 10, 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                //* Title text
+                                Text(
+                                  data[index].title,
+                                  style: TextStyle(
+                                    fontFamily: 'poppins',
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color(0xff707070),
+                                  ),
+                                ),
+                                Wrap(
+                                  children: [
+                                    //* Location
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Icon(
+                                          Icons.location_on,
+                                          color: Color(0xff1976D2),
+                                          size: 20,
+                                        ),
+                                        SizedBox(width: 2),
+                                        Text(
+                                          data[index].eventLocation,
+                                          style: TextStyle(
+                                            fontFamily: 'poppins',
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w400,
+                                            color: Color(0xff1976D2),
+                                          ),
+                                        ),
+                                        SizedBox(width: 20),
+                                      ],
+                                    ),
+                                    //* Date
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Icon(
+                                          Customicons.calendar_alt,
+                                          color: Color(0xff1976D2),
+                                          size: 18,
+                                        ),
+                                        SizedBox(width: 5),
+                                        Text(
+                                          DateFormat('MMMM d, yyyy')
+                                              .format(DateFormat('MM/dd/yyyy')
+                                                  .parse(data[index]
+                                                      .eventStartDate))
+                                              .toString(),
+                                          style: TextStyle(
+                                            fontFamily: 'poppins',
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w400,
+                                            color: Color(0xff1976D2),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
+    );
   }
 }
